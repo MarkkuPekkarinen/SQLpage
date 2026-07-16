04a94ddf31efcf39c692dcd9aed8683f2d8770fa
Ophir LOJKINE <contact@ophir.dev>
2026-07-16 17:16:03 +0200

    fix mysql incompatibility in tests

--- tests/sql_test_files/data/coalesce_short_circuit.sql
+++ tests/sql_test_files/data/coalesce_short_circuit.sql
@@ -4,4 +4,4 @@ from (select 'cached' as cached_response) cache_row;
 
 select '%20' as expected,
     coalesce(cached_response, sqlpage.url_encode(' ')) as actual
-from (select cast(null as varchar(16)) as cached_response) cache_row;
+from (select null as cached_response) cache_row;
