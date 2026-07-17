71d0837117c1a7a417abcee1f8f7600ff842c43f
Ophir LOJKINE <contact@ophir.dev>
2026-07-17 21:44:52 +0200

    Link email results to Mailpit

--- examples/sending emails/send-advanced.sql
+++ examples/sending emails/send-advanced.sql
@@ -20,10 +20,12 @@ select 'shell' as component, 'Advanced email result' as title;
 
 select
     'alert' as component,
+    'send-result' as id,
     case when json_extract($result, '$.status') = 'accepted' then 'success' else 'danger' end as color,
     case when json_extract($result, '$.status') = 'accepted' then 'Email sent successfully' else 'Email could not be sent' end as title,
     json_extract($result, '$.error') as description;
 
 select 'button' as component;
+select 'Open Mailpit inbox' as title, 'http://localhost:8025' as link, '_blank' as target, 'inbox' as icon, 'primary' as color;
 select 'Send another advanced email' as title, 'advanced.sql' as link;
 select 'Back to examples' as title, 'index.sql' as link, 'secondary' as color;
